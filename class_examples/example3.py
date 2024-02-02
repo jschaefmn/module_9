efa15de8c4ecac96019b956e3e907fede6844322
jschaefMB <jschaefmn@gmail.com>
2024-02-01 19:39:07 -0600

    yes

--- class_examples/example3.py
+++ class_examples/example3.py
@@ -4,6 +4,19 @@ person = {
   "age": 35
 }
 
-popped_item = person.pop("first_name")
+# popped_item = person.pop("first_name")
 
-print(person)
+# print(person)
+# print(popped_item)
+
+# for key in person:
+#   print(person[key])
+
+person_list = []
+
+for k in person:
+  person_list.append(person.pop[k])
+
+person.clear()
+print(person_list) # [jake, schaefbauer, 24]
+print(person) #{}
---
+++ class_examples/example4.py
@@ -0,0 +1,7 @@
+my_list = [i**2 for i in range(10)]
+print(my_list)
+
+# {0:0, 1:1, 2:4, ..., 9:81}
+
+my_dict = {num:num**2 for num in range(10)}
+print(my_dict)
