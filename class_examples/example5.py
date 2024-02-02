c93da720adf73119240e7ad6d81a93904dc24593
jschaefMB <jschaefmn@gmail.com>
2024-02-01 20:12:39 -0600

    finished 2 examples

---
+++ class_examples/example5.py
@@ -0,0 +1,9 @@
+new_list = [1, 2, 3, 4, 4, 3, 2, 1]
+
+# new set = set(new_list)
+new_set = set([1, 2, 3, 4, 4, 3, 2, 1])
+for i in range(10,30):
+  new_set.add(i)
+  
+print(new_set)
+  
---
+++ class_examples/example6.py
@@ -0,0 +1,12 @@
+set1 = set([0,2,4,6,8])
+set2 = set([0,3,6,9])
+
+# new_set = set1.union(set2)
+# new_set = set1.intersection(set2)
+# print(new_set)
+
+print(set1)
+
+set3 = set([6,8,10,12])
+set1.update(set3)
+print(set1)
