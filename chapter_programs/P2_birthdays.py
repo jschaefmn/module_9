7519fdf5ea4be35a3d9546eb3919a6d97686094f
jakeschaef <jschaefmn@gmail.com>
2024-02-08 11:27:37 -0600

    finished program 2

---
+++ chapter_programs/P2_birthdays.py
@@ -0,0 +1,103 @@
+# This program uses a dictionary to keep friends'
+# names and birthdays
+
+# Global constants for menu choices
+LOOK_UP = 1
+ADD = 2
+CHANGE = 3
+DELETE = 4
+QUIT = 5
+
+# main function
+def main():
+  # Create an empty dictionary
+  birthdays = {}
+  
+  # initialize a variable for user's choice
+  choice = 0
+  
+  while choice != QUIT:
+    # get user's menu choice
+    choice = get_menu_choice()
+    
+    # process the choice
+    if choice == LOOK_UP:
+      look_up(birthdays)
+    elif choice == ADD:
+      add(birthdays)
+    elif choice == CHANGE:
+      change(birthdays)
+    elif choice == DELETE:
+      delete(birthdays)
+      
+# The get_menu_choice function displays the menu
+# and gets a validated choice from the user
+def get_menu_choice():
+  print()
+  print('Friends and Their Birthdays')
+  print('-----------------------')
+  print('1. Look up a birthday')
+  print('2. Add a new birthday')
+  print('3. Change a birthday')
+  print('4. Delete a birthday')
+  print('5. Quit the program')
+  print()
+  
+  # get user's choice
+  choice = int(input('Enter your choice: '))
+  
+  # Validate the choice
+  while choice < LOOK_UP or choice > QUIT:
+    choice = int(input('Enter a valid choice: '))
+    
+  # return user's choice
+  return choice
+
+# The look_up function looks up a name in the birthdays dictionary
+def look_up(birthdays):
+  # Get a name to look up
+  name = input('Enter a name: ')
+  
+  # Look it up in the dictionary
+  print(birthdays.get(name, 'Not Found.'))
+  
+# the add function adds a new entry into the birthdays dictionary
+def add(birthdays):
+  # get name and birthday
+  name = input('Enter a name: ')
+  bday = input('Enter a birthday: ')
+  
+  if name not in birthdays:
+    birthdays[name] = bday
+  else:
+    print('That entry already exists.')
+    
+# The change function changes an existing entry
+# in the birthdays dictionary
+def change(birthdays):
+  # get name to look up
+  name = input('enter a name: ')
+  
+  if name in birthdays:
+    bday = input('Enter a new birthday: ')
+    
+    # Update the entry
+    birthdays[name] = bday
+  else:
+    print('That name is not found.')
+    
+# The delete function deletes an entry from the
+# birthdays dictionary
+def delete(birthdays):
+  # get name to look up
+  name = input('Enter a name: ')
+  
+  # if name is found, delete entry
+  if name in birthdays:
+    del birthdays[name]
+  else:
+    print('That name is not found.')
+    
+# Call main function
+if __name__ == '__main__':
+  main()
